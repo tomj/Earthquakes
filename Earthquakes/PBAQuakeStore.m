e286ba3fe8441bd810332f466bfc772f7e1fbf7f
Pouria Almassi <pouria@bignerdranch.com>
2014-11-05 17:09:50 -0800

    add step by step break down of the nsurlsession

--- Earthquakes/PBAQuakeStore.m
+++ Earthquakes/PBAQuakeStore.m
@@ -23,34 +23,39 @@ NSString * const PBAQuakeStoreBaseURLString = @"http://earthquake-report.com/fee
     return sharedStore;
 }
 
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-
-    }
-    return self;
-}
-
 - (void)downloadDataWithCompletion:(void (^)(NSArray *quakes, NSError *error))completion
 {
     NSMutableArray *quakeData = [NSMutableArray array];
-
-    // returns a shared singleton session object. The shared session uses the currently set
-    //global NSURLCache, NSHTTPCookieStorage, and NSURLCredentialStorage objects and is based on the default configuration.
-    NSURLSession *sess = [NSURLSession sharedSession];
-
-    // reuse base url
+    
+    // step 1: define path
     NSString *path = [NSString stringWithFormat:@"%@%@", PBAQuakeStoreBaseURLString, @"recent-eq?json"];
+    
+    // step 2: create instance of NSURLRequest
     NSURL *url = [NSURL URLWithString:path];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
-    // create a data (download) task
-    NSURLSessionDataTask *task = [sess dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-
+    // step 3: get instance of NSURLSession
+    //
+    // returns a shared singleton session object.
+    // The shared session uses the currently set
+    // global NSURLCache, NSHTTPCookieStorage,
+    // and NSURLCredentialStorage objects and is based
+    // on the default configuration.
+    NSURLSession *sess = [NSURLSession sharedSession];
+    
+    // step 4: create a data (download) task
+    NSURLSessionDataTask *task =
+    [sess dataTaskWithRequest:request completionHandler:^(NSData *data,
+                                                          NSURLResponse *response,
+                                                          NSError *error) {
+        
         if (!error) {
-            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-
+            // step 6: serialize JSON data from the NSData object returned
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                 options:0
+                                                                   error:&error];
+            
+            // step 7: do stuff with retrieved JSON
             for (NSDictionary *d in json) {
                 PBAQuake *quake = [[PBAQuake alloc] init];
                 quake.location = [d objectForKey:@"location"];
@@ -61,10 +66,10 @@ NSString * const PBAQuakeStoreBaseURLString = @"http://earthquake-report.com/fee
                 quake.latitude = [[d objectForKey:@"latitude"] floatValue];
                 quake.longitude = [[d objectForKey:@"longitude"] floatValue];
                 quake.dateTime = [d objectForKey:@"date_time"];
-
+                
                 [quakeData addObject:quake];
             }
-
+            
             if (completion) {
                 completion(quakeData, nil);
             }
@@ -75,6 +80,8 @@ NSString * const PBAQuakeStoreBaseURLString = @"http://earthquake-report.com/fee
             }
         }
     }];
+    
+    // step 5: session tasks begin in the suspended state. we must resume them
     [task resume];
 }
 
