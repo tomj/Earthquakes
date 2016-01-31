b7739afd3a2541a81fd745eabad872f120dd3869
Pouria B. Almassi <pouriaba@gmail.com>
2016-01-31 17:00:19 +1000

    Initial set up of simple datasource class.

--- Earthquakes/PBADataSource.m
+++ Earthquakes/PBADataSource.m
@@ -7,7 +7,39 @@
 //
 
 #import "PBADataSource.h"
+#import "Quake.h"
+
+@interface PBADataSource ()
+
+@property (nonatomic, copy) NSArray *objects;
+
+@end
 
 @implementation PBADataSource
+
+- (instancetype)initWithObjects:(NSArray *)objects;
+{
+    self = [super init];
+    if (self) {
+        _objects = objects;
+    }
+    return self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.objects ? self.objects.count : 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TempCell" forIndexPath:indexPath];
+
+    Quake *q = [self.objects objectAtIndex:indexPath.row];
+
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ / %@", q.location, q.magnitude];
+
+    return cell;
+}
 
 @end
