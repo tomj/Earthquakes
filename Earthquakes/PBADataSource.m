503727a3ffa7dc89db994891d649349c1ea76cb9
Pouria B. Almassi <pouriaba@gmail.com>
2016-02-10 16:25:26 +1000

    Allow passing of the cell reuse identifier to the DataSource class.

--- Earthquakes/PBADataSource.m
+++ Earthquakes/PBADataSource.m
@@ -8,20 +8,24 @@
 
 #import "PBADataSource.h"
 #import "Quake.h"
+#import "PBAListTableViewCell.h"
 
 @interface PBADataSource ()
 
 @property (nonatomic, copy) NSArray *objects;
+@property (nonatomic, copy) NSString *identifier;
 
 @end
 
 @implementation PBADataSource
 
-- (instancetype)initWithObjects:(NSArray *)objects;
+- (instancetype)initWithObjects:(NSArray *)objects
+                     identifier:(NSString *)identifier;
 {
     self = [super init];
     if (self) {
         _objects = objects;
+        _identifier = identifier;
     }
     return self;
 }
@@ -33,13 +37,26 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TempCell" forIndexPath:indexPath];
+    PBAListTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:self.identifier forIndexPath:indexPath];
 
     Quake *q = [self.objects objectAtIndex:indexPath.row];
 
-    cell.textLabel.text = [NSString stringWithFormat:@"%@ / %@", q.location, q.magnitude];
+    cell.locationLabel.text = q.location;
+    cell.dateLabel.text = [[self dateFormatter] stringFromDate:q.dateTime];
+    cell.magnitudeLabel.text = [NSString stringWithFormat:@"%@", q.magnitude];
 
     return cell;
+}
+
+- (NSDateFormatter *)dateFormatter
+{
+    static NSDateFormatter *dateFormatter;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateFormat = @"YYYY MM dd";
+    });
+    return dateFormatter;
 }
 
 @end
