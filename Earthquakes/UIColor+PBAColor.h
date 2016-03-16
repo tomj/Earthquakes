ff25200cbc1ab249429650a6a679729c040af70c
Pouria B. Almassi <pouriaba@gmail.com>
2016-03-17 09:48:11 +1000

    Add a customized blue color and category.

---
+++ Earthquakes/UIColor+PBAColor.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+PBAColor.h
+//  Earthquakes
+//
+//  Created by Pouria Almassi on 3/17/16.
+//  Copyright © 2016 Pouria Almassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (PBAColor)
+
++ (UIColor *)pba_blueColor;
+
+@end
---
+++ Earthquakes/UIColor+PBAColor.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+PBAColor.m
+//  Earthquakes
+//
+//  Created by Pouria Almassi on 3/17/16.
+//  Copyright © 2016 Pouria Almassi. All rights reserved.
+//
+
+#import "UIColor+PBAColor.h"
+
+@implementation UIColor (PBAColor)
+
++ (UIColor *)pba_blueColor;
+{
+    return [UIColor colorWithRed:0.200 green:0.478 blue:0.718 alpha:1.00];
+}
+
+@end
